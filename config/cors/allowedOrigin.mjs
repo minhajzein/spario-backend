9fe51ac06043d886f81b286c9ae8e864290347a2
minhajgrohance <minhaj.grohance@gmail.com>
2025-04-11 20:32:58 +0530

    domain added in origins

--- config/cors/allowedOrigin.mjs
+++ config/cors/allowedOrigin.mjs
@@ -1,3 +1,3 @@
-const allowedOrigins = ['http://localhost:5173', 'http://spariocp.com']
+const allowedOrigins = ['http://localhost:5173', 'http://spariocp.com', 'https://www.spariocp.com']
 
 export default allowedOrigins
