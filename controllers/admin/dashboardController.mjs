417ba9be8ff1a0837e01afcda4fb44b729f91610
minhajgrohance <minhaj.grohance@gmail.com>
2025-04-13 20:24:47 +0530

    dashboard added

--- controllers/admin/dashboardController.mjs
+++ controllers/admin/dashboardController.mjs
@@ -1,8 +1,39 @@
+import Transaction from '../../models/transactionModel.mjs'
+import Store from '../../models/storeModel.mjs'
+import Executive from '../../models/userModel.mjs'
+
+
 export const getDashboard = async (req, res) => {
     try {
-        const currentFinancialYear = new Date().getFullYear()
-        console.log(currentFinancialYear)
-        res.json(currentFinancialYear)
+        const totolOutstanding = await Transaction.find({ entry: 'debit' }).aggregate([
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: '$amount' }
+                }
+            }
+        ])
+
+        const totalPaid = await Transaction.find({ entry: 'credit' }).aggregate([
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: '$amount' }
+                }
+            }
+        ]);
+        const totalStores = await Store.find().countDocuments()
+        const totalExecutives = await Executive.find().countDocuments()
+        res.send({
+            success: true,
+            data: {
+                totalOutstanding: totolOutstanding[0] ? totolOutstanding[0].total : 0,
+                totalPaid: totalPaid[0] ? totalPaid[0].total : 0,
+                totalStores: totalStores,
+                totalExecutives: totalExecutives
+            }
+        })
+
     } catch (error) {
         console.log(error)
         res.send({ success: false, message: 'Internal server error' })
